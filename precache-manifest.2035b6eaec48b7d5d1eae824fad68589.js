654d4bd5c6b93e0d3bc7d4ac1cc9807150544db4
Riccardo Corsini <122900895+corsone@users.noreply.github.com>
2024-03-07 16:54:41 +0100

    Updates

---
+++ precache-manifest.2035b6eaec48b7d5d1eae824fad68589.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f971ed54ae5fda3acaaac36c02cdfd3",
+    "url": "/corsoneWebSite/index.html"
+  },
+  {
+    "revision": "7f0e24b2bb07617a105b",
+    "url": "/corsoneWebSite/static/css/main.7b300a98.chunk.css"
+  },
+  {
+    "revision": "48f1534304f7f150fc0f",
+    "url": "/corsoneWebSite/static/js/2.a221d85d.chunk.js"
+  },
+  {
+    "revision": "2133a33f4e9282dbf343",
+    "url": "/corsoneWebSite/static/js/3.c60c86f6.chunk.js"
+  },
+  {
+    "revision": "7f0e24b2bb07617a105b",
+    "url": "/corsoneWebSite/static/js/main.698e7cdf.chunk.js"
+  },
+  {
+    "revision": "820061675983d7be6092",
+    "url": "/corsoneWebSite/static/js/runtime~main.d15e8538.js"
+  },
+  {
+    "revision": "81e0d8a058b25d733684a982a04543a0",
+    "url": "/corsoneWebSite/static/media/Instagram_Glyph_Black.81e0d8a0.svg"
+  },
+  {
+    "revision": "8d399960b5774e2f549c0d7c386454c0",
+    "url": "/corsoneWebSite/static/media/Instagram_Glyph_White.8d399960.svg"
+  },
+  {
+    "revision": "a0b00583d93c2f7084ad151ee0849934",
+    "url": "/corsoneWebSite/static/media/github-mark-white.a0b00583.svg"
+  },
+  {
+    "revision": "8dcc6b5262f3b6138b1566b357ba89a9",
+    "url": "/corsoneWebSite/static/media/github-mark.8dcc6b52.svg"
+  },
+  {
+    "revision": "0bd4acfd763a614f0d1de6a5dc7682c1",
+    "url": "/corsoneWebSite/static/media/me.0bd4acfd.PNG"
+  },
+  {
+    "revision": "8c7f3dbd88f8957faee1fb66fe5f60e0",
+    "url": "/corsoneWebSite/static/media/me2.8c7f3dbd.jpg"
+  }
+]);
